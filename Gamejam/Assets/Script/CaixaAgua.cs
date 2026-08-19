e8e3dbcff72727d85c3b27f0e8d7e9afab2355a2
glg37 <bielgutjahr1@gmail.com>
2026-08-19 15:01:25 -0300

    falta pouco

--- Gamejam/Assets/Script/CaixaAgua.cs
+++ Gamejam/Assets/Script/CaixaAgua.cs
@@ -4,6 +4,9 @@ public class CaixaAgua : MonoBehaviour
 {
     private ArmaAgua armaDoJogador;
 
+    [Header("Mensagem na tela")]
+    [SerializeField] private GameObject mensagemRecarregar;
+
     private void OnTriggerEnter(Collider other)
     {
         ArmaAgua arma = other.GetComponentInChildren<ArmaAgua>();
@@ -11,6 +14,10 @@ public class CaixaAgua : MonoBehaviour
         if (arma != null)
         {
             armaDoJogador = arma;
+
+            // Mostra a mensagem
+            mensagemRecarregar.SetActive(true);
+
             Debug.Log("Entrou na caixa d'água");
         }
     }
@@ -22,13 +29,16 @@ public class CaixaAgua : MonoBehaviour
         if (arma != null)
         {
             armaDoJogador = null;
+
+            // Esconde a mensagem
+            mensagemRecarregar.SetActive(false);
+
             Debug.Log("Saiu da caixa d'água");
         }
     }
 
     void Update()
     {
-        
         if (armaDoJogador != null && Input.GetKey(KeyCode.R))
         {
             armaDoJogador.RecarregarAgua();
